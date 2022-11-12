08b48de5654b7f07867f1ecbd6f53f581a9ceb7e
Georgios Botsoglou <80923325+georbots@users.noreply.github.com>
2022-11-12 16:33:16 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 echo "Initiating data_collection"
-python src/data_collection.py
+python3 src/data_collection.py
 echo "Data collection finished"
 
 echo "Initiating data processing"
-python src/data_preprocessing.py
+python3 src/data_preprocessing.py
 echo "Data processing finished"
 
 echo "Visualizing data"
-python src/visualization.pycd
+python3 src/visualization.pycd
